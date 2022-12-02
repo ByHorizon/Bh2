5940f160b5024d413d534397e1845126982526ba
ByHorizon <rptedpa4@naver.com>
2022-12-02 16:35:33 +0900

    Initial commit

---
+++ src/main/java/Bh2Application.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bh2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bh2Application.class, args);
+	}
+
+}
---
+++ src/test/java/reality/Bh2/Bh2ApplicationTests.java
@@ -0,0 +1,13 @@
+package reality.Bh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bh2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
